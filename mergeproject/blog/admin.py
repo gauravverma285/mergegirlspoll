32a7a37580b0a14c8a9e8f3b8592b3086b800ded
gauravverma285 <gaurav998290@gmail.com>
2022-10-14 19:49:28 +0530

    sec commit

--- mergeproject/blog/admin.py
+++ mergeproject/blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post
 from django.contrib.auth.admin import UserAdmin
-
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
